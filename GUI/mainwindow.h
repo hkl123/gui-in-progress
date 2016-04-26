b0660fbda7d288357a5074f0233b5f16318fb4eb
hkl123 <hkline@stetson.edu>
2016-04-26 13:35:10 -0400

    changes

--- GUI/mainwindow.h
+++ GUI/mainwindow.h
@@ -43,6 +43,10 @@ private slots:
 
     void on_pushButton_2_clicked();
 
+    void on_pushButton_5_clicked();
+
+    void on_pushButton_6_clicked();
+
 private:
     Ui::MainWindow *ui;
     QString str;
